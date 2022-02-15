1422a5c1efe5ccdca8af4823d56a26d11cdc8f75
Damien Sedgwick <damiensedgiwck@icloud.com>
2022-02-15 19:36:59 +0000

    Completed 'Digital Root' challenge

--- golang/digitalRoot/main.go
+++ golang/digitalRoot/main.go
@@ -3,10 +3,41 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
+func MakeSlice(n int) []int {
+	var s []int
+	str := strconv.Itoa(n)
+	strSlice := strings.Split(str, "")
+	for _, v := range strSlice {
+		num, _ := strconv.Atoi(v)
+		s = append(s, num)
+	}
+
+	return s
+}
+
+func SumSliceDigits(s []int) int {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+
+	if len(MakeSlice(sum)) == 1 {
+		return sum
+	}
+
+	return SumSliceDigits(MakeSlice(sum))
+}
+
+func DigitalRoot(n int) int {
+	return SumSliceDigits(MakeSlice(n))
+}
+
 func main() {
-	fmt.Println("Digital Root")
+	fmt.Println(DigitalRoot(493193))
 }
 
 // Best codewars solution
--- golang/digitalRoot/main_test.go
+++ golang/digitalRoot/main_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMakeSlice(t *testing.T) {
+	assert.Equal(t, []int{2, 3}, MakeSlice(23))
+	assert.Equal(t, []int{5, 6, 7}, MakeSlice(567))
+	assert.Equal(t, []int{9, 8, 7, 6, 5}, MakeSlice(98765))
+}
+
+func TestSumSliceDigits(t *testing.T) {
+	assert.Equal(t, 7, SumSliceDigits([]int{1, 6}))
+	assert.Equal(t, 6, SumSliceDigits([]int{9, 4, 2}))
+	assert.Equal(t, 6, SumSliceDigits([]int{1, 3, 2, 1, 8, 9}))
+	assert.Equal(t, 2, SumSliceDigits([]int{4, 9, 3, 1, 9, 3}))
+}
