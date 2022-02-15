2983aad73f8498d82f280a6b813dd4aa69a1c080
Damien Sedgwick <damiensedgiwck@icloud.com>
2022-02-15 19:00:10 +0000

    Added new challenge 'Digital Root' and amended folder names

---
+++ golang/digitalRoot/main.go
@@ -0,0 +1,12 @@
+// link to kata https://www.codewars.com/kata/541c8630095125aba6000c00/train/go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Digital Root")
+}
+
+// Best codewars solution
