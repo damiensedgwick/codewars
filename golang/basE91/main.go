c329c31baf39e37e76dadf6f9eb46bd446631c2a
Damien Sedgwick <damiensedgiwck@icloud.com>
2022-02-17 08:23:14 +0000

    Added new challenge 'BasE91 encoding and decoding'

---
+++ golang/basE91/main.go
@@ -0,0 +1,12 @@
+// link to kata https://www.codewars.com/kata/58a57c6bcebc069d7e0001fe
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("BasE91 Encoding and Decoding")
+}
+
+// Simplest / top solution on Codewars
