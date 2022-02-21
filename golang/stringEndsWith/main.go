8dce1524f3dfc4f20e23d85e6266961e786ae342
Damien Sedgwick <damiensedgiwck@icloud.com>
2022-02-21 17:21:08 +0000

    Added string ends with challenge

---
+++ golang/stringEndsWith/main.go
@@ -0,0 +1,12 @@
+// link to kata https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("String ends with")
+}
+
+// Best codewars solution
